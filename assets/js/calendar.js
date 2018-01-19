a0e107c5a76257f088e643f19b3187eb044adced
Dmytro Soroka <diem9@yandex.ru>
2018-01-19 14:13:01 +0200

    created basic a project structure

---
+++ assets/js/calendar.js
@@ -0,0 +1,7 @@
+// create module
+var myCalendar = angular.module('myCalendar', []);
+
+// create controller
+myCalendar.controller('calendarController', function($scope) {
+  $scope.test = "testff";
+});
